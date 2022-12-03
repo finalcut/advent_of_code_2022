27715d8ed9417b08efc58d667cfd73f09281590c
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-03 13:07:39 -0500

    use rustfmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,28 +1,25 @@
 use std::{
-  env,
-  fs::File,
-  io::{self, BufRead, BufReader},
-  path::Path,
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
 };
 
+fn main() -> std::io::Result<()> {
+    let path = env::current_dir()?;
+    let input_file = path.display().to_string() + "/values.txt";
 
+    let input = lines_from_file(input_file).expect("Could not load values");
 
-fn main() -> std::io::Result<()>  {
+    let output = split_delimited(&input, &"".to_owned());
 
-  let path = env::current_dir()?;
-  let input_file = path.display().to_string() + "/values.txt";
+    let mut sum_vec: Vec<u32> = Vec::with_capacity(output.len());
 
-  let input = lines_from_file(input_file).expect("Could not load values");
-
-  let output = split_delimited(&input, &"".to_owned());
-
-  let mut sum_vec : Vec<u32> = Vec::with_capacity(output.len());
-
-  for &i in &output {
-    let number_vec: Vec<u32> = i.iter().flat_map(|x| x.parse()).collect(); // converts each of the sub vectors into numbers from strings and removes any values that can't become numbers (blanks)
-    let sum : u32 = number_vec.iter().sum();
-    sum_vec.push(sum);
-  }
+    for &i in &output {
+        let number_vec: Vec<u32> = i.iter().flat_map(|x| x.parse()).collect(); // converts each of the sub vectors into numbers from strings and removes any values that can't become numbers (blanks)
+        let sum: u32 = number_vec.iter().sum();
+        sum_vec.push(sum);
+    }
 
     sum_vec.sort();
     sum_vec.reverse();
@@ -30,7 +27,7 @@ fn main() -> std::io::Result<()>  {
     println!("Richest Elf: {:?}", sum_vec[0]);
 
     sum_vec.truncate(3);
-    let top3 : u32 = sum_vec.iter().sum();
+    let top3: u32 = sum_vec.iter().sum();
     println!("Top Three Elves: {:?}", top3);
 
     Ok(())
@@ -57,5 +54,5 @@ where
 
 // function found at https://stackoverflow.com/a/35820003
 fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-  BufReader::new(File::open(filename)?).lines().collect()
+    BufReader::new(File::open(filename)?).lines().collect()
 }
