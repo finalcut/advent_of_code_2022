c2a76e6289fe2b028edc3dc6a03ceccbb2d95473
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-04 10:53:12 -0500

    clean up conversion to numbers

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,4 +1,4 @@
-use aoc_util::{get_seed_data};
+use aoc_util::{get_seed_data,};
 
 fn main() {
   let input = get_seed_data().expect("Could not load values");
@@ -6,28 +6,27 @@ fn main() {
   let mut consumed_count = 0;
   let mut overlapped_count = 0;
   for i in input {
-    let parts : Vec<&str> = i.split(",").collect();
 
-    let sect1 : Vec<&str> = parts[0].split("-").collect();
-    let s1: Vec<u32> = sect1.iter().flat_map(|x| x.parse()).collect();
+    // standardize my delimiter
+    let x = str::replace(&i, "-", ",");
 
-    let sect2 : Vec<&str> = parts[1].split("-").collect();
-    // converts each of the sub vectors into numbers from strings and removes any values that can't become numbers (blanks)
-    let s2: Vec<u32> = sect2.iter().flat_map(|x| x.parse()).collect();
+    //convert to vector of numbers
+    let section_borders: Vec<i32> = x
+        .split(",")
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
 
-
-    if section_fully_includes_section(&s1,&s2){
+    if section_fully_includes_section(&section_borders){
       consumed_count += 1;
-    } else if section_fully_includes_section(&s2,&s1){
+    } else if section_fully_includes_section(&section_borders){
       consumed_count += 1;
     }
 
-    if section_overlaps_section(&s1,&s2){
+    if section_overlaps_section(&section_borders){
       overlapped_count += 1;
-    } else if section_overlaps_section(&s2,&s1){
+    } else if section_overlaps_section(&section_borders){
       overlapped_count += 1;
     }
-
   }
 
   println!("part1: {}", consumed_count);
@@ -36,10 +35,10 @@ fn main() {
 
 }
 
-fn section_fully_includes_section(s1: &Vec<u32>, s2: &Vec<u32>) -> bool {
-  return s1[0] <= s2[0] && s1[1] >= s2[1];
+fn section_fully_includes_section(section_borders: &Vec<i32>) -> bool {
+  return section_borders[0] <= section_borders[2] && section_borders[1] >=section_borders[3];
 }
 
-fn section_overlaps_section(s1: &Vec<u32>, s2: &Vec<u32>) -> bool {
-  return s2[0] <= s1[1] && s2[1] >= s1[1];
+fn section_overlaps_section(section_borders: &Vec<i32>) -> bool {
+  return section_borders[2] <= section_borders[1] && section_borders[3] >= section_borders[0];
 }
