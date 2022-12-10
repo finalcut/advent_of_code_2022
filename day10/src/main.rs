e679d97a2a0ed17e021bcc8d5dfe496567f12245
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-10 12:42:01 -0500

    finish part 2 and reformat

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,11 +6,21 @@ struct Signal {
     after: i64,
 }
 
+struct Row {
+    pixels: Vec<char>,
+}
+
+struct Grid {
+    rows: Vec<Row>,
+}
+
 fn main() {
     let data = read_file("values.txt").expect("Could not load values");
 
     let mut x_register_value: Vec<i64> = Vec::new(); // tracking register value at the end of each cycle
     let mut signal_strength: Vec<Signal> = Vec::new();
+
+    let mut grid = get_empty_grid(40, 6);
 
     x_register_value.push(1);
     signal_strength.push(Signal {
@@ -53,8 +63,6 @@ fn main() {
                 after: compute_strength(r2, cycle),
             };
 
-            //println!("ADDR cycle: {}, x: {}, D: {}, A: {}", cycle, r2, sig.during, sig.after);
-
             signal_strength.push(sig);
             x_register_value.push(r2);
         }
@@ -62,6 +70,10 @@ fn main() {
 
     let part1 = sum_cycles(&signal_strength, [20, 60, 100, 140, 180, 220].to_vec());
     println!("part1: {:?}", part1);
+
+    // part 2
+    light_up_grid(&mut grid, &x_register_value);
+    draw_grid(&grid);
 }
 
 fn compute_strength(register: i64, cycle: usize) -> i64 {
@@ -74,4 +86,60 @@ fn sum_cycles(signals: &Vec<Signal>, points: Vec<usize>) -> i64 {
         sum += signals[point].during;
     }
     return sum;
+}
+
+fn get_empty_grid(width: i16, height: i16) -> Grid {
+    let mut grid = Grid { rows: Vec::new() };
+
+    for _i in 0..height {
+        let mut row = Row { pixels: Vec::new() };
+        for _x in 0..width {
+            /*
+             instructions said to use a . for this; but it makes the final screen hard to read
+             so I used a {space} and the rendering is MUCH easier to read
+            */
+            row.pixels.push(' ');
+        }
+        grid.rows.push(row);
+    }
+    return grid;
+}
+
+fn draw_grid(grid: &Grid) {
+    for row in &grid.rows {
+        let pix = row.pixels.clone();
+        let _line: String = pix.into_iter().collect();
+        println!("{:?}", _line);
+    }
+}
+
+fn light_up_grid(grid: &mut Grid, x_values: &Vec<i64>) {
+    let width = grid.rows[0].pixels.len();
+
+    println!("{:?}", x_values);
+
+    for g in 0..grid.rows.len() {
+        let start = g * width;
+        let end = start + width;
+
+        let mut point: i64 = 0;
+        for cycle in start..end {
+            /*
+                  we always want the register value DURING the cycle not at the end
+                  since we start with a zero based index this works perfectly relative
+                  to how I stored register values at each index for the end of that cycle.
+                  so in x_values[1] it has the value for the end of cycle 1.. the register value
+                  during cycle 1 wwould be at x_values[0]
+                  i got kind of lucky there.
+            */
+            let x = x_values[cycle];
+            let sprite_pos = [x - 1, x + 1];
+
+            if point >= sprite_pos[0] && point <= sprite_pos[1] {
+                grid.rows[g].pixels[point as usize] = '#';
+            }
+
+            point += 1;
+        }
+    }
 }
