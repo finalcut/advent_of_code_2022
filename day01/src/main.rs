02c6582eaffa058dc23617381ff9c29761af6fa8
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-12 18:28:48 -0500

    fix file name being read in day1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 
 fn main() {
-    let totals: Vec<u64> = include_str!("../input.txt") // reads in the whole file into a single line
+    let totals: Vec<u64> = include_str!("../values.txt") // reads in the whole file into a single line
         .split("\n\n") // creates a vector of groups of number strings
         .map(|elf| {
             elf.split('\n') // creates a vector of numbers from each line in the group
