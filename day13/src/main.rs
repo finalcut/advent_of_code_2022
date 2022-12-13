ec6ebd543a258f6760c34c97ed1bcb494ab2757d
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-13 18:11:54 -0500

    part 1 is working.. now how the heck do I sort this vector?

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,40 +2,79 @@
   Notes:
     numbers as single character strings such as "6" and "5" have standard comparison operations like < and > and act just like their numeric counterparts
 
-
+failure: 1646 too small
 */
 fn main() {
+    let input: Vec<Vec<&str>> = include_str!("../values.txt")
+        .split("\n\n")
+        .map(|block| block.trim())
+        .map(|block| { block.split("\n").collect() })
+        .collect();
 
-  let x = "[1,[2,[3,[4,[5,6,7]]]],8,9]";
-  let y = "[1,[2,[3,[4,[5,6,0]]]],8,9]";
+    let mut part1_good_indexes: Vec<usize> = [].to_vec();
+    for c in 0..input.len() {
+        let x = json::parse(input[c][0]).unwrap();
+        let y = json::parse(input[c][1]).unwrap();
 
-  let jx = json::parse(x).unwrap();
-  let jy = json::parse(y).unwrap();
 
-  let mut correct = false;
-
-  println!("{:?}",jx[0].is_number());
-  if jx.len() != jy.len() {
-    for i in 0..jx.len() {
-      let left = jx[i].clone();
-      let right= jy[i].clone();
-
-      if left.is_number() && right.is_number() {
-        if left.as_i16().unwrap() > right.as_i16().unwrap() {
-          break; // bigger number on right than on left.. not ordered right.
+        if compare_arrays(x,y) == 1 {
+          part1_good_indexes.push(c + 1);
         }
-      }
-
-      if left.is_number() && right.is_array() {
-        let left = json::array![left.as_i16().unwrap()];
-
-      }
     }
- }
+
+    let mut flat : Vec<&str> = input.concat();
+    flat.push("[[2]]");
+    flat.push("[[6]]");
+   // flat.sort_by(|a,b| compare(a,b) );
+
+
+    let sum: usize = part1_good_indexes.iter().sum();
+    println!("part 1: {:?}", sum);
+    println!("indexes of success {:?}", part1_good_indexes);
+}
+
+fn compare(x: json::JsonValue, y: json::JsonValue) -> i16 {
+    out_fails("x/y", x.clone(), y.clone());
+
+
+    if x.is_array() && y.is_array() {
+      let mut max = x.len();
+      if y.len() > max {
+        max = y.len();
+      }
+
+      for i in 0..max {
+        if i >= x.len() {return 1};
+        if i >= y.len() {return -1};
+
+        let result = compare(x[i].clone() ,y[i].clone());
+        if result != 0 {
+          return result;
+        }
+
+      }
+
+      return 0; // they are the same..
+
+  } else if x.is_number() & y.is_number() {
+    if x.as_i16().unwrap() > y.as_i16().unwrap() { return -1 };
+    if y.as_i16().unwrap() > x.as_i16().unwrap() { return 1 };
+    return 0; // they are the same
+  } else {
+    if x.is_array() {
+      return compare_arrays(x, json::array![y.as_i16().unwrap()]);
+    } else {
+      return compare_arrays(json::array![x.as_i16().unwrap()] , y)
+    }
+  }
+
+
 
 }
 
-fn is_list_less_than_list(x : json::JsonValue, y: json::JsonValue) -> bool {
-
-  return false;
+fn out_fails(m: &str, a: json::JsonValue, b: json::JsonValue) {
+    println!("msg: {}",  m);
+    println!("{} {:?}", a.len(), json::stringify(a.clone()));
+    println!("{} {:?}", b.len(), json::stringify(b.clone()));
+    println!();
 }
