01b24ee0e66e065603fb3b03732398c4500ad6e0
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-13 08:29:31 -0500

    starting day 13.  trick here is going to be parsing input.

---
+++ day13/src/main.rs
@@ -0,0 +1,9 @@
+/*
+  Notes:
+    numbers as single character strings such as "6" and "5" have standard comparison operations like < and > and act just like their numeric counterparts
+
+
+*/
+fn main() {
+    println!("Hello, world!");
+}
