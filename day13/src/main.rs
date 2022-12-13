10e819b96efc6aee5b19d6665681bb1ea2c832db
Bill Rawlinson <bill.rawlinson@mheducation.com>
2022-12-13 11:39:51 -0500

    start day13; exploring json parsing

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -5,5 +5,37 @@
 
 */
 fn main() {
-    println!("Hello, world!");
+
+  let x = "[1,[2,[3,[4,[5,6,7]]]],8,9]";
+  let y = "[1,[2,[3,[4,[5,6,0]]]],8,9]";
+
+  let jx = json::parse(x).unwrap();
+  let jy = json::parse(y).unwrap();
+
+  let mut correct = false;
+
+  println!("{:?}",jx[0].is_number());
+  if jx.len() != jy.len() {
+    for i in 0..jx.len() {
+      let left = jx[i].clone();
+      let right= jy[i].clone();
+
+      if left.is_number() && right.is_number() {
+        if left.as_i16().unwrap() > right.as_i16().unwrap() {
+          break; // bigger number on right than on left.. not ordered right.
+        }
+      }
+
+      if left.is_number() && right.is_array() {
+        let left = json::array![left.as_i16().unwrap()];
+
+      }
+    }
+ }
+
+}
+
+fn is_list_less_than_list(x : json::JsonValue, y: json::JsonValue) -> bool {
+
+  return false;
 }
